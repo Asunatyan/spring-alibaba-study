6a5423c0e253822f25d8784c04d20f41f1381bbb
tamakiakoo <tamakiakoo@163.com>
2022-06-08 22:20:53 +0800

    add

--- vip-spring-cloud-alibaba/mall-user-ribbon-demo/src/main/java/com/tuling/mall/ribbondemo/MallUserRibbonDemoApplication.java
+++ vip-spring-cloud-alibaba/mall-user-ribbon-demo/src/main/java/com/tuling/mall/ribbondemo/MallUserRibbonDemoApplication.java
@@ -1,13 +1,9 @@
 package com.tuling.mall.ribbondemo;
 
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
-import com.tuling.mall.rule.RibbonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClientName;
-import org.springframework.cloud.netflix.ribbon.RibbonClients;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class,
         DruidDataSourceAutoConfigure.class})
